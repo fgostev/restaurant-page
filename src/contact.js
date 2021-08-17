aa3a7ae8346d545e3ead25ca6adc9194fe1a2d69
Filipp Gostev <fgostev@gmail.com>
2021-08-16 23:08:27 -0500

    base design done

--- src/contact.js
+++ src/contact.js
@@ -4,6 +4,10 @@ function loadContact(){
     const h1 = document.createElement('h1');
     h1.textContent = 'Find us at:'
     mainContent.appendChild(h1);
+    const img = document.createElement('img');
+    img.alt = 'arcos';
+    img.src = '../imgs/queretaro.jpeg';
+    mainContent.appendChild(img);
 }
 
 export default loadContact;
--- src/home.js
+++ src/home.js
@@ -1,21 +1,23 @@
+import loadMenu from './menu.js';
+
 function loadHome(){   
     const main = document.getElementById('main');
     main.textContent = '';
     const h1 = document.createElement('h1');
-    h1.textContent = 'Bienvenidos a La Nogaderia!';
+    h1.textContent = 'La Nogaderia!';
     main.appendChild(h1);
-    const orderNow = document.createElement('a');
-    orderNow.href = '#menu';
-    orderNow.textContent = 'Order now!';
-    main.appendChild(orderNow);
+    const hr = document.createElement('hr');
+    main.appendChild(hr);
     const para = document.createElement('p');
     para.id = 'description';
     para.textContent = "Best Chili Nogadas in town. Can't miss them!"
     main.appendChild(para);
-    const img = document.createElement('img');
-    img.alt = 'nogada';
-    img.src = '../imgs/Chile_nogada.png';
-    main.appendChild(img);
+    const orderNow = document.createElement('a');
+    orderNow.id = 'orderNow';
+    orderNow.textContent = 'Order Now';
+    orderNow.addEventListener('click', loadMenu)
+    main.appendChild(orderNow);
+
 }
 
 export default loadHome;
